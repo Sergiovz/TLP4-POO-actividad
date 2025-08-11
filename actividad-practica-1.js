5a8e8ab11e904ebb1f76d48ab0069c8cb76410d2
sergiovz <sergiovaldez599@gmail.com>
2025-08-11 15:01:29 -0300

    modificación del archivo, actualizado POO basado a prototype

--- actividad-practica-1.js
+++ actividad-practica-1.js
@@ -1,21 +1,33 @@
 //* Actividad Practica Punto 1
-function CuentaBancaria() {
+function CuentaBancaria(titular, saldoInicial) {
   this.titular = titular;
   this.saldoInicial = saldoInicial;
-
-  depositar = function (monto) {
-    this.saldoInicial += monto;
-  };
-
-  extraer = function (monto) {
-    if (this.saldoInicial >= monto) {
-      this.saldoInicial -= monto;
-    } else {
-      console.log("Lo siento, fondos insuficientes");
-    }
-  };
-
-  consultarSaldo = function () {
-    console.log(`Saldo actual: ${this.saldoInicial}`);
-  };
 }
+
+CuentaBancaria.prototype.depositar = function (monto) {
+  if (monto > 0) {
+    this.saldoInicial += monto;
+    console.log(`Depósito realizado. Nuevo saldo: $${this.saldoInicial}`);
+  } else {
+    console.log("El monto debe ser mayor a 0");
+  }
+};
+
+CuentaBancaria.prototype.extraer = function (monto) {
+  if (this.saldoInicial >= monto) {
+    this.saldoInicial -= monto;
+  } else {
+    console.log("Lo siento, fondos insuficientes");
+  }
+};
+
+CuentaBancaria.prototype.consultarSaldo = function () {
+  console.log(`Titular: ${this.titular} - Saldo actual: ${this.saldoInicial}`);
+};
+
+const cuenta = new CuentaBancaria("Jhon", 1000);
+
+cuenta.consultarSaldo();
+cuenta.extraer(200);
+cuenta.consultarSaldo();
+cuenta.depositar(1000);
